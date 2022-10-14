e733be9f00ceeb91edcf90b63475e4e095e11a93
olukola6868 <akanbib29@gmail.com>
2022-10-13 22:53:20 -1200

    olukola code challenge

---
+++ number 1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace Number1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter your first number:");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your second number:");
+            int num2 = int.Parse(Console.ReadLine());
+            CheckNumber(num1, num2);
+
+
+        }
+        public static int CheckNumber(int num1, int num2)
+        {
+            int Length = 0;
+            if (num1 == 0 || num2 == 0)
+            {
+                return 0;
+            }
+            int count = 0;
+            while (num1 != 0 && num2 != 0)
+            {
+                for (int i = 0; i < Length; i++)
+                {
+                    if (num1 > num2)
+                    {
+                        num1 -= num2;
+                    }
+                    else
+                    {
+                        num2 -= num1;
+                    }
+                    count++;
+                }
+
+            }
+            return count;
+        }
+
+    }
+}
---
+++ number 2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Challenge
+{
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter your wrod");
+            string word = Console.ReadLine();
+            ConvertWord(word);
+        }
+        static void ConvertWord(string word)
+        {
+            char[]charArray = word.ToCharArray();
+            int length = charArray.Length;
+            for(int i = 0;i < word.Length;i++)
+            {
+                int count = 0;
+                count ++;
+                Console.WriteLine(count);
+            }
+        }
+    }
+}
